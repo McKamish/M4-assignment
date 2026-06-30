2efe119b983bf14f22bcd231a5cdd2084582e004
Cory Jameson <corymjameson@gmail.com>
2026-06-30 16:55:02 -0700

    functions practice complete

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -1,9 +1,31 @@
 //STEP 1
 
+function halfNumber(num) {
+    let half = num / 2
+    return `Half of ${num} is ${half}`
+}
+
 //STEP 2
+
+function squareNumber(num) {
+    let square = num ** 2
+    return `The result of squaring the number ${num} is ${square}`
+}
 
 //STEP 3
 
+function percentOf(num1, num2) {
+    let percent = (num1 / num2) * 100
+    return `${num1} is ${percent}% of ${num2}`
+}
+percentOf(14,60)
+
 //STEP 4
 
+function findModulus(num1, num2) {
+    let modu = num1 % num2
+    return `${modu} is the modulus of ${num1} and ${num2}`
+}
+
 //STEP 5
+//There is no step 5 in the instructions
